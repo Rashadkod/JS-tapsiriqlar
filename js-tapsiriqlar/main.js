4d9201a9cc9823fd1da50e6271237e87baa9333f
Rashadkod <rashad.hasanov0@gmail.com>
2024-12-05 11:31:49 +0400

    İlk commit

---
+++ js-tapsiriqlar/main.js
@@ -0,0 +1,25 @@
+// 1 first
+//let a=prompt("Adinizi qeyd edin");
+//alert ( "Salam" + " " + a) 
+// 2 second
+// let a=prompt("Sevdiyiniz reng")
+// document.write("Senin sevdiyin reng" + " " + a + "dir")
+// 3 third
+//let a=prompt("Senin nece yasin var")
+//console.log("Senin" + " " + a + " " + "yasin var")
+// 4 fourth
+//let a=prompt("1ci ededi daxil edin")
+//let b=prompt("2ci ededi daxil edin")
+//let sum=Number(a)+Number(b)
+//document.write(sum)
+// 5 fifth
+//let a=prompt("Adinizi daxil edin")
+//alert ("Salam" + " " + a)
+//console.log ("Salam" + " " + a)
+//document.write ("Salam" + " " + a)
+//let a=prompt("Basligi daxil edin")
+//let b=prompt("Metni daxil edin")
+//document.write("<h1>" + a + "</h1>");
+//document.write("<p>" + b + "</p>"); 
+//console.log(a)
+//console.log(b)
